782dffdc7ef7ccb192f67eefd45c631ceb9f4419
Zara9626 <63889471+Zara9626@users.noreply.github.com>
2022-11-16 11:22:36 -0800

    Add files via upload

---
+++ src/components/Sidebar/contractors.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Contractors() {
+  return (
+    <div className="contractors">
+      <h1>Contractors  i am here</h1>
+    </div>
+  );
+}
+
+export default Contractors;
